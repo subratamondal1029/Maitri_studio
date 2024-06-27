62b749657a7d73f8af0733f6d6ddf73e4a628392
subrata mondal <subratamondal1029@outlook.com>
2024-06-27 19:25:31 +0530

    adding scroll indicator

--- src/script.js
+++ src/script.js
@@ -85,3 +85,10 @@ function swiperActive(){
       },
 })
 }
+
+window.addEventListener("scroll", () =>{
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollIndicator").style.width = scrolled + "%";
+})
