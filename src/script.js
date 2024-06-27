c816a82e45cf3eadb9624b91ffac47361f94afc5
subrata mondal <subratamondal1029@outlook.com>
2024-06-27 09:12:47 +0530

    styling is complete

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
 const swiper = new Swiper(".mySwiper", {
     loop: true,
+    grabCursor: true,
+    centeredSlides: true,
     autoplay: {
         delay: 1500,
         disableOnInteraction: false,
       },
+})
+
+
+document.getElementById('currentYear').textContent = new Date().getFullYear()
+
+document.getElementById('goTop').addEventListener('click', () =>{
+ window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 })
